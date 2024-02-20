54dccfc7764579e91ac0841c8979621642cb5954
Bruno Cordeiro <brunocordeiro180@gmail.com>
2024-02-20 20:28:07 -0300

    Exercise 1 with 100% of coverage

--- unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
+++ unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
@@ -3,5 +3,39 @@ package fizzbuzz
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
-	t.Skip("TODO: Add tests")
+
+	checkResults := func(t testing.TB, got, want []string) {
+		t.Helper()
+
+		if len(got) != len(want) {
+			t.Errorf("got %d words, but wanted %d", len(got), len(want))
+		}
+
+		for i, g := range got {
+			if want[i] != g {
+				t.Errorf("got %v words, but wanted %v", got, want)
+			}
+		}
+	}
+
+	t.Run("Just Fizz", func(t *testing.T) {
+		want := []string{"Fizz"}
+		got := FizzBuzz(1, 1, 10)
+
+		checkResults(t, got, want)
+	})
+
+	t.Run("Just Buzz", func(t *testing.T) {
+		want := []string{"Buzz"}
+		got := FizzBuzz(1, 10, 1)
+
+		checkResults(t, got, want)
+	})
+
+	t.Run("Fizz and Buzz", func(t *testing.T) {
+		want := []string{"1", "2", "Fizz", "4", "Buzz"}
+		got := FizzBuzz(5, 3, 5)
+
+		checkResults(t, got, want)
+	})
 }
