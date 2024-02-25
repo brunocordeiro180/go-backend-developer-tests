c5e1834fd0e44b4ca9001c32e38ceb478bba2f1a
Bruno Cordeiro <brunocordeiro180@gmail.com>
2024-02-25 00:39:41 -0300

    Exercise 2 complete

--- rest-service/main.go
+++ rest-service/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/stackpath/backend-developer-tests/rest-service/pkg/handlers"
+	"net/http"
 )
 
 func main() {
@@ -9,4 +12,15 @@ func main() {
 	fmt.Println()
 
 	// TODO: Add RESTful web service here
+	r := mux.NewRouter()
+	r.HandleFunc("/people", handlers.PeopleHandler).
+		Methods("GET")
+	r.HandleFunc("/people", handlers.PeopleHandler).
+		Queries("first_name", "{first_name}").
+		Queries("last_name", "{last_name}")
+	r.HandleFunc("/people", handlers.PeopleHandler).
+		Queries("phone_number", "{phone_number}")
+	r.HandleFunc("/people/{id}", handlers.PeopleByIdHandler)
+
+	http.ListenAndServe(":8010", r)
 }
