23e7c68b9334fc6d915c567e6b63bd82a49101b8
Bruno Cordeiro <brunocordeiro180@gmail.com>
2024-02-27 21:31:37 -0300

    Complete exercise 3

--- input-processing/main.go
+++ input-processing/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,7 +12,16 @@ func main() {
 	fmt.Println()
 
 	// Read STDIN into a new buffered reader
-	reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewScanner(os.Stdin)
 
-	// TODO: Look for lines in the STDIN reader that contain "error" and output them.
+	for reader.Scan() {
+		line := reader.Text()
+		if strings.Contains(strings.ToLower(line), "error") {
+			fmt.Println(line)
+		}
+
+		if err := reader.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "error reading standard input:", err)
+		}
+	}
 }
